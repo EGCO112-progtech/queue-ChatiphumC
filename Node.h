4b8bbce90a972230ffca8008547b9083c42ade15
ChatiphumC <156408486+ChatiphumC@users.noreply.github.com>
2024-02-27 06:52:40 +0000

    rod fai bon far

--- Node.h
+++ Node.h
@@ -19,18 +19,34 @@ typedef struct node* NodePtr;
 
 void enqueue(NodePtr * head, NodePtr* tail, int x){
   Node* new_node=(NodePtr) malloc(sizeof(Node));
-if(new_node){ 
-    /* Finish queue*/
- }
+  if(new_node){ 
+    new_node->data = x;
+    new_node->nextPtr = NULL;
+    if(*head == NULL)
+    {
+      *head = new_node;
+    }
+    else
+    {
+      (*tail)->nextPtr = new_node;
+    }
+    *tail = new_node;
+  }
+
 }
 
 
 int dequeue(NodePtr* head, NodePtr* tail){
-  NodePtr t=*head;
-   if(t){
-   int value= t->data;
-   /* Finish dequeue*/
-       
+  NodePtr tempPtr=*head;
+   if(tempPtr){
+   int value= tempPtr->data;
+   
+   *head = (*head)->nextPtr;
+   if(*head == NULL)
+   {
+    *tail = NULL;
+   }
+   free(tempPtr);
        
    return value;
    }
